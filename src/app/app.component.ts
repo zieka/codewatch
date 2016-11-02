ca93c194956bbdcc7db57e483b027c367d36d2f7
Kyle Scully <(none)>
2016-11-01 23:39:23 -0700

    added format style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp, database } from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+    // Initialize Firebase
+    let config = {
+      apiKey: "AIzaSyAcyKj4sATgCl2cOP4FqtK4LRV-MfQ9Zqk",
+      authDomain: "codewatch-56eb1.firebaseapp.com",
+      databaseURL: "https://codewatch-56eb1.firebaseio.com",
+      storageBucket: "codewatch-56eb1.appspot.com",
+      messagingSenderId: "350421315980"
+    };
+    initializeApp(config);
+
+    let root = database().ref('messages');
+
+    root.on('value', function(snap) {
+      console.log(snap.key, snap.val())
+    });
+  }
 }
